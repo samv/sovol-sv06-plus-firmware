9fad72e0ea935dfa74eab0ec4643cf57b419ec42
daleye <daleye@gmail.com>
2023-12-14 21:13:58 -0800

    Add M73 support to show print progress on the SV06 Plus LCD
    Imported from the LCD branch on daleye's fork:
    
    https://github.com/daleye/SV06-PLUS/compare/42f16c0fdb5e7de022d615e0a665aed042e0be49...daleye:SV06-PLUS:b5ec92a7a4487f183714fc544ca0ef5fd8cb180a

--- MarlinSV06Plus/Marlin/Configuration_adv.h
+++ MarlinSV06Plus/Marlin/Configuration_adv.h
@@ -1298,7 +1298,7 @@
   //#define LCD_DECIMAL_SMALL_XY
 
   // Add an 'M73' G-code to set the current percentage
-  //#define LCD_SET_PROGRESS_MANUALLY
+  #define LCD_SET_PROGRESS_MANUALLY
 
   // Show the E position (filament used) during printing
   //#define LCD_SHOW_E_TOTAL
@@ -1306,10 +1306,10 @@
 
 // LCD Print Progress options
 #if EITHER(SDSUPPORT, LCD_SET_PROGRESS_MANUALLY)
-  #if ANY(HAS_MARLINUI_U8GLIB, EXTENSIBLE_UI, HAS_MARLINUI_HD44780, IS_TFTGLCD_PANEL, IS_DWIN_MARLINUI)
-    //#define SHOW_REMAINING_TIME         // Display estimated time to completion
+  #if ANY(HAS_MARLINUI_U8GLIB, EXTENSIBLE_UI, HAS_MARLINUI_HD44780, IS_TFTGLCD_PANEL, IS_DWIN_MARLINUI,RTS_AVAILABLE)
+    #define SHOW_REMAINING_TIME         // Display estimated time to completion
     #if ENABLED(SHOW_REMAINING_TIME)
-      //#define USE_M73_REMAINING_TIME    // Use remaining time from M73 command instead of estimation
+      #define USE_M73_REMAINING_TIME    // Use remaining time from M73 command instead of estimation
       //#define ROTATE_PROGRESS_DISPLAY   // Display (P)rogress, (E)lapsed, and (R)emaining time
     #endif
   #endif
@@ -3836,7 +3836,7 @@
 #define HOST_ACTION_COMMANDS
 #if ENABLED(HOST_ACTION_COMMANDS)
   //#define HOST_PAUSE_M76
-  //#define HOST_PROMPT_SUPPORT
+  #define HOST_PROMPT_SUPPORT
   //#define HOST_START_MENU_ITEM  // Add a menu item that tells the host to start
 #endif
 
--- MarlinSV06Plus/Marlin/src/gcode/lcd/M73.cpp
+++ MarlinSV06Plus/Marlin/src/gcode/lcd/M73.cpp
@@ -30,6 +30,10 @@
 
 #if ENABLED(DWIN_CREALITY_LCD_ENHANCED)
   #include "../../lcd/e3v2/enhanced/dwin.h"
+#elif ENABLED(RTS_AVAILABLE)
+  unsigned char percentComplete = 0;
+  uint16_t timeRemaining = 0;
+  #include "../../lcd/sv06p/LCD_RTS.h"
 #endif
 
 
@@ -50,11 +54,30 @@ void GcodeSuite::M73() {
     if (parser.seenval('P'))
       ui.set_progress((PROGRESS_SCALE) > 1
         ? parser.value_float() * (PROGRESS_SCALE)
-        : parser.value_byte()
-      );
+        : parser.value_byte());
+      #if ENABLED(RTS_AVAILABLE)
+        percentComplete = parser.value_byte();
+        rtscheck.RTS_SndData((unsigned char)percentComplete, PRINT_PROCESS_ICON_VP);
+        rtscheck.RTS_SndData((unsigned char)percentComplete, PRINT_PROCESS_VP);
+        if (percentComplete == 100)
+        {
+          rtscheck.RTS_SndData(0, PRINT_TIME_HOUR_VP);
+          rtscheck.RTS_SndData(0, PRINT_TIME_MIN_VP);
+          rtscheck.RTS_SndData(0, PRINT_SURPLUS_TIME_HOUR_VP);
+          rtscheck.RTS_SndData(0, PRINT_SURPLUS_TIME_MIN_VP);
+          rtscheck.RTS_SDcard_Stop();
+          PrintFlag = 0;
+          Update_Time_Value = 0;
+        }
+      #endif
 
     #if ENABLED(USE_M73_REMAINING_TIME)
-      if (parser.seenval('R')) ui.set_remaining_time(60 * parser.value_ulong());
+      if (parser.seenval('R')) ui.set_remaining_time(parser.value_ulong());
+      #if ENABLED(RTS_AVAILABLE)
+        timeRemaining = parser.value_ulong();
+        rtscheck.RTS_SndData((timeRemaining / 60), PRINT_SURPLUS_TIME_HOUR_VP);
+        rtscheck.RTS_SndData((timeRemaining % 60), PRINT_SURPLUS_TIME_MIN_VP);
+      #endif
     #endif
 
   #endif
